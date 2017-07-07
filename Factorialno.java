02003fdd36f5b7813030c098dd3a69a3c71a48c7
731614105013 <gomathiammupk29@gmail.com>
2017-07-07 15:30:53 +0530

    Create Factorialno.java

---
+++ Factorialno.java
@@ -0,0 +1,17 @@
+# factorialno
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Factorialno
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int fact=1;
+int a=sc.nextInt();
+for(int i=1;i<=a;i++)
+{
+fact=fact*i;
+}
+}
+}
